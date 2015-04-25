95e7371cde579c20dfc7b048d53e7c2f8f7979a9
gjr3205 <grule1540@gmail.com>
2015-04-25 16:45:14 -0400

    Added Blinky code

---
+++ TestCode/Debug/blink.pp
@@ -0,0 +1,15 @@
+# FIXED
+
+blink.obj: ../blink.c
+blink.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+blink.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430fr5969.h
+blink.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+blink.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics.h
+blink.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics_legacy_undefs.h
+
+../blink.c: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430fr5969.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics_legacy_undefs.h: 
